40870be323a0d98be422ecf7c9e1f1decf4c93f6
Eugene Fedoseev <abap@sapville.com>
2019-12-20 18:26:55 +0000

    Add translatable texts

--- walkthrough/js/controller/App.controller.js
+++ walkthrough/js/controller/App.controller.js
@@ -3,9 +3,10 @@ sap.ui.define(
   [
     'sap/ui/core/mvc/Controller',
     'sap/m/MessageToast',
-    'sap/ui/model/json/JSONModel'
+    'sap/ui/model/json/JSONModel',
+    'sap/ui/model/resource/ResourceModel'
   ],
-  function(Controller, MessageToast, JSONModel) {
+  function(Controller, MessageToast, JSONModel, ResourceModel) {
     'use strict';
     return Controller.extend('sapui.demo.walkthrough.js.controller.App', {
       onInit: function() {
@@ -17,12 +18,15 @@ sap.ui.define(
         this.getView().setModel(
           new JSONModel(oData)
         );
+        this.getView().setModel(
+          new ResourceModel({ bundleName: 'sapui.demo.walkthrough.i18n.i18n' }),
+          'i18n'
+        );
       },
       onShowHello: function() {
-        MessageToast.show(
-          `Hello ${this.getView().getModel().getProperty('/recipient/name')}`
-        );
-
+        const sRecipient = this.getView().getModel().getProperty('/recipient/name');
+        const sText = this.getView().getModel('i18n').getResourceBundle().getText('helloMsg', [sRecipient]);
+        MessageToast.show(sText);
       }
     });
   }
