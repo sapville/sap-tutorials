7ae5b4e9b3ae7b18d36b6ae52e018ea629e97e35
Eugene Fedoseev <abap@sapville.com>
2019-12-31 00:40:57 +0000

    Add Fragment and Dialog

--- walkthrough/js/controller/HelloPanel.controller.js
+++ walkthrough/js/controller/HelloPanel.controller.js
@@ -2,15 +2,32 @@
 sap.ui.define(
   [
     'sap/ui/core/mvc/Controller',
-    'sap/m/MessageToast'
+    'sap/m/MessageToast',
+    'sap/ui/core/Fragment'
   ],
-  function(Controller, MessageToast, JSONModel, ResourceModel) {
+  function(Controller, MessageToast, Fragment) {
     'use strict';
     return Controller.extend('sapui.demo.walkthrough.js.controller.HelloPanel', {
       onShowHello: function() {
         const sRecipient = this.getView().getModel().getProperty('/recipient/name');
         const sText = this.getView().getModel('i18n').getResourceBundle().getText('helloMsg', [sRecipient]);
         MessageToast.show(sText);
+      },
+      onOpenDialog: function() {
+        const oView = this.getView();
+        const oLoadedDialog = this.byId('helloDialog');
+        if (!oLoadedDialog) {
+          Fragment.load({
+            id: oView.getId(),
+            name: 'sapui.demo.walkthrough.view.HelloDialog'
+          }).then(oDialog => {
+            oView.addDependent(oDialog);
+            oDialog.open();
+          });
+        }
+        else {
+          oLoadedDialog.open();
+        }
       }
     });
   }
