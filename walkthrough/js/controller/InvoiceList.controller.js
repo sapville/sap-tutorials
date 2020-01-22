55aeb5f801db8f7f38f9e93d449d68625805038f
Eugene Fedoseev <abap@sapville.com>
2020-01-22 18:09:08 +0000

    Expression Binding

--- walkthrough/js/controller/InvoiceList.controller.js
+++ walkthrough/js/controller/InvoiceList.controller.js
@@ -8,9 +8,26 @@ sap.ui.define(
     'use strict';
     return Controller.extend('sapui.demo.walkthrough.js.controller.InvoiceList', {
       onInit: function() {
+        this._threshold = {
+          min: 10,
+          max: 100
+        };
         this.getView().setModel(
-          new JSONModel({ currency: 'CAD' }),
-          'view');
+          new JSONModel({
+            currency: 'CAD',
+            threshold: this._threshold
+          }), 'view');
+      },
+      getState: function(amount) {
+        if (amount < this._threshold.min) {
+          return 'Error';
+        }
+        else if (amount >= this._threshold.min && amount < this._threshold.max) {
+          return 'Information';
+        }
+        else {
+          return 'Warning';
+        }
       }
     });
   }
