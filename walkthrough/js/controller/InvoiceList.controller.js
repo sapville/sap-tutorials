fce05477685a2ee1c515b28245843fa387637948
Eugene Fedoseev <abap@sapville.com>
2020-01-27 04:12:07 +0000

    Add filtering

--- walkthrough/js/controller/InvoiceList.controller.js
+++ walkthrough/js/controller/InvoiceList.controller.js
@@ -3,18 +3,30 @@ sap.ui.define(
   [
     'sap/ui/core/mvc/Controller',
     'sap/ui/model/json/JSONModel',
-    'sapui/demo/walkthrough/model/formatter'
+    'sapui/demo/walkthrough/model/formatter',
+    'sap/ui/model/Filter',
+    'sap/ui/model/FilterOperator'
   ],
-  function(Controller, JSONModel, formatter) {
+  function(Controller, JSONModel, formatter, Filter, FilterOperator) {
     'use strict';
     return Controller.extend('sapui.demo.walkthrough.js.controller.InvoiceList', {
       formatter: formatter,
       onInit: function() {
         this.getView().setModel(
           new JSONModel({
-            currency: 'CAD'
+            currency: 'CAD',
+            threshold: formatter.threshold
           }), 'view');
       },
+      onFilterInvoices: function(oEvent) {
+        const aFilter = [];
+        const sQuery = oEvent.getSource().getValue();
+        // const sQuery = oEvent.getParameter('query'); - for search method. No list live update, search when Enter is hit
+        if (sQuery && sQuery.length > 0) {
+          aFilter.push(new Filter('ProductName', FilterOperator.Contains, sQuery));
+        }
+        this.byId('invoiceList').getBinding('items').filter(aFilter);
+      }
     });
   }
 );
