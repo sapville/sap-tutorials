3ece5ec21122d90e2d6551a4e285f3b4e1ea8a06
Eugene Fedoseev <abap@sapville.com>
2020-02-04 20:25:29 +0000

    Add routing back

--- walkthrough/controller/Detail.controller.js
+++ walkthrough/controller/Detail.controller.js
@@ -2,14 +2,23 @@
 sap.ui.define(
   [
     'sap/ui/core/mvc/Controller',
-    'sap/ui/core/UIComponent'
+    'sap/ui/core/UIComponent',
+    'sap/ui/core/routing/History'
   ],
-  function(Controller, UIComponent) {
+  function(Controller, UIComponent, History) {
     'use strict';
     return Controller.extend('sapui.demo.walkthrough.controller.Detail', {
       onInit: function() {
         const oRouter = UIComponent.getRouterFor(this);
         oRouter.getRoute('detail').attachPatternMatched(this._onObjectMatched, this);
+      },
+      onNavBack: function() {
+        if (History.getInstance().getPreviousHash() !== undefined) {
+          window.history.go(-1);
+        }
+        else {
+          UIComponent.getRouterFor(this).navTo('overview', {}, true);
+        }
       },
       _onObjectMatched: function(oEvent) {
         this.getView().bindElement({
