5aa97e86b8026e7c8d9e673d38e881a3c9425cfc
Eugene Fedoseev <abap@sapville.com>
2020-07-23 20:57:01 +0000

    dtbnd: Property binding

--- data-binding/webapp/Component.js
+++ data-binding/webapp/Component.js
@@ -1,15 +1,30 @@
 /*globals sap*/
 sap.ui.define(
-  ['sap/ui/core/UIComponent'],
-  function(UIComponent) {
+  [
+    'sap/ui/core/UIComponent',
+    'sap/ui/model/json/JSONModel',
+    'sap/ui/model/BindingMode'
+  ],
+  function(UIComponent, JSONModel, BindingMode) {
     'use strict';
     return UIComponent.extend('tutorial.dataBinding.Component', {
       metadata: {
         manifest: 'json'
       },
       init: function() {
+
         UIComponent.prototype.init.apply(this, arguments);
-        console.log('Component');
+
+        const oModel = new JSONModel({
+          firstName: 'Eugene',
+          lastName: 'Fedoseev',
+          enabled: true,
+          panelHeaderText: 'Data Binding Basics'
+        });
+
+        // oModel.setDefaultBindingMode(BindingMode.OneWay);
+
+        this.setModel(oModel);
       }
     });
   }
--- data-binding/webapp/controller/App.controller.js
+++ data-binding/webapp/controller/App.controller.js
@@ -2,9 +2,5 @@
 sap.ui.define([
   'sap/ui/core/mvc/Controller'
 ], function(Controller) {
-  return Controller.extend('tutorial.dataBinding.controller.App', {
-    onInit: function() {
-      console.log('onInit');
-    }
-  });
+  return Controller.extend('tutorial.dataBinding.controller.App', {});
 });
