ed22cf92c0c7167aaddd272361dc31a678052524
Eugene Fedoseev <abap@sapville.com>
2020-07-24 22:24:38 +0000

    dtbnd: Message manager

--- data-binding/webapp/controller/App.controller.js
+++ data-binding/webapp/controller/App.controller.js
@@ -4,6 +4,9 @@ sap.ui.define([
   'sap/m/library'
 ], function(Controller, mobileLib) {
   return Controller.extend('tutorial.dataBinding.controller.App', {
+    onInit: function() {
+      sap.ui.getCore().getMessageManager().registerObject(this.getView(), true);
+    },
     formatMail: function(sFName, sLName) {
       const oResourceModel = this.getView().getModel('i18n').getResourceBundle();
       return mobileLib.URLHelper.normalizeEmail(
