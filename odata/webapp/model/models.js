3e28e5bf83e4d600de9076687fa562e72425b089
Eugene Fedoseev <abap@sapville.com>
2020-08-10 20:53:22 +0000

    odata: initial commit

---
+++ odata/webapp/model/models.js
@@ -0,0 +1,17 @@
+/*globals sap*/
+sap.ui.define([
+  'sap/ui/model/json/JSONModel',
+  'sap/ui/Device'
+], function(JSONModel, Device) {
+  'use strict';
+
+  return {
+
+    createDeviceModel: function() {
+      var oModel = new JSONModel(Device);
+      oModel.setDefaultBindingMode('OneWay');
+      return oModel;
+    }
+
+  };
+});
