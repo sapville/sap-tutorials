558732d4efc02d2300e3a931f38a5a0cca0d5e04
Eugene Fedoseev <abap@sapville.com>
2020-10-28 21:46:18 +0000

    nav: no hash navigaion

--- navigation/webapp/controller/Home.controller.js
+++ navigation/webapp/controller/Home.controller.js
@@ -6,6 +6,10 @@ sap.ui.define([
 
   return BaseController.extend('sap.ui.demo.nav.controller.Home', {
 
+    onDisplayNotFound: function() {
+      this.getRouter().getTargets().display('notFound', { fromTarget: 'home' });
+    }
+
   });
 
 });
--- navigation/webapp/controller/NotFound.controller.js
+++ navigation/webapp/controller/NotFound.controller.js
@@ -6,8 +6,21 @@ sap.ui.define([
 
   return BaseController.extend('sap.ui.demo.nav.controller.NotFound', {
     onInit: function() {
+      this.getRouter().getTarget('notFound').attachDisplay(
+        oEvent => this._oData = oEvent.getParameter('data')
+      );
+    },
 
+    onNavBack: function() {
+      if (this._oData && this._oData.fromTarget) {
+        this.getRouter().getTargets().display(this._oData.fromTarget);
+        delete this._oData.fromTarget;
+      }
+      else {
+        BaseController.prototype.onNavBack.apply(this, arguments);
+      }
     }
+
   });
 
 });
