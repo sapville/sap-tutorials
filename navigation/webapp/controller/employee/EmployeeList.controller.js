cfb619e67c7477630ab88320387536450489958d
Eugene Fedoseev <abap@sapville.com>
2020-10-29 22:46:16 +0000

    nav: static pattern

---
+++ navigation/webapp/controller/employee/EmployeeList.controller.js
@@ -0,0 +1,9 @@
+/*globals sap*/
+sap.ui.define([
+  'sap/ui/demo/nav/controller/BaseController'
+], function(BaseController) {
+  'use strict';
+  return BaseController.extend('sap.ui.demo.nav.controller.employee.EmployeeList', {
+
+  });
+});
